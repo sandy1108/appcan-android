e7dac608534df15ab889604ba42c4ffac8aed8db
jiangpingping <1626794969@qq.cm>
2015-08-13 15:54:58 +0800

    修复WebView的JS对象注入漏洞问题

--- Engine/src/org/zywx/wbpalmstar/engine/CBrowserMainFrame.java
+++ Engine/src/org/zywx/wbpalmstar/engine/CBrowserMainFrame.java
@@ -19,13 +19,29 @@
 package org.zywx.wbpalmstar.engine;
 
 
-import android.app.Activity;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+import org.zywx.wbpalmstar.engine.universalex.EUExBase;
+import org.zywx.wbpalmstar.engine.universalex.EUExManager;
+import org.zywx.wbpalmstar.engine.universalex.EUExScript;
+import org.zywx.wbpalmstar.engine.universalex.ThirdPluginMgr;
+import org.zywx.wbpalmstar.engine.universalex.ThirdPluginObject;
+import org.zywx.wbpalmstar.widgetone.WidgetOneApplication;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
 import android.webkit.ValueCallback;
@@ -36,6 +52,7 @@ import android.widget.EditText;
 public class CBrowserMainFrame extends WebChromeClient {
 
     public Context mContext;
+    private boolean mIsInjectedJS;
 
 	/**
 	 *android version < 2.1 use 
@@ -46,11 +63,29 @@ public class CBrowserMainFrame extends WebChromeClient {
 
 	@Override
 	public void onProgressChanged(WebView view, int newProgress) {
-		EBrowserView target = (EBrowserView)view;
-		EBrowserWindow bWindow = target.getBrowserWindow();
-		bWindow.setGlobalProgress(newProgress);
-		if(100 == newProgress){
-			bWindow.hiddenProgress();
+		if (view != null) {
+
+	        //为什么要在这里注入JS
+	        //1 OnPageStarted中注入有可能全局注入不成功，导致页面脚本上所有接口任何时候都不可用
+	        //2 OnPageFinished中注入，虽然最后都会全局注入成功，但是完成时间有可能太晚，当页面在初始化调用接口函数时会等待时间过长
+	        //3 在进度变化时注入，刚好可以在上面两个问题中得到一个折中处理
+	        //为什么是进度大于25%才进行注入，因为从测试看来只有进度大于这个数字页面才真正得到框架刷新加载，保证100%注入成功
+			if (newProgress <= 25) {
+				mIsInjectedJS = false;
+			} else if (!mIsInjectedJS) {
+				view.loadUrl(EUExScript.F_UEX_SCRIPT);
+				Log.i("jiangpingping","CBrowserMainFrame onProgressChanged F_UEX_SCRIPT "+EUExScript.F_UEX_SCRIPT);
+				mIsInjectedJS = true;
+			}
+	        
+			EBrowserView target = (EBrowserView)view;
+			EBrowserWindow bWindow = target.getBrowserWindow();
+			if (bWindow != null) {
+				bWindow.setGlobalProgress(newProgress);
+				if (100 == newProgress) {
+					bWindow.hiddenProgress();
+				}
+			}
 		}
 	}
 
@@ -102,38 +137,98 @@ public class CBrowserMainFrame extends WebChromeClient {
 
 	@Override
 	public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, final JsPromptResult result) {
-		if (!((EBrowserActivity)view.getContext()).isVisable()){
+		if (message != null
+				&& message.startsWith(EUExScript.JS_APPCAN_ONJSPARSE)) {
 			result.cancel();
-			return true;
-		}
-		AlertDialog.Builder dia = new AlertDialog.Builder(view.getContext());
-		dia.setTitle(null);
-		dia.setMessage(message);
-		final EditText input = new EditText(view.getContext());
-		if (defaultValue != null) {
-			input.setText(defaultValue);
-		}
-		input.setSelectAllOnFocus(true);
-		dia.setView(input);
-		dia.setCancelable(false);
-		dia.setPositiveButton("确定", 
-			new DialogInterface.OnClickListener() {
+			handleUexFunction(view,
+					message.substring(EUExScript.JS_APPCAN_ONJSPARSE.length()));
+		} else {
+			if (!((EBrowserActivity) view.getContext()).isVisable()) {
+				result.cancel();
+				return true;
+			}
+			AlertDialog.Builder dia = new AlertDialog.Builder(view.getContext());
+			dia.setTitle(null);
+			dia.setMessage(message);
+			final EditText input = new EditText(view.getContext());
+			if (defaultValue != null) {
+				input.setText(defaultValue);
+			}
+			input.setSelectAllOnFocus(true);
+			dia.setView(input);
+			dia.setCancelable(false);
+			dia.setPositiveButton("确定", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					result.confirm(input.getText().toString());
-			}
-		});
-		dia.setNegativeButton("取消", 
-			new DialogInterface.OnClickListener() {
+				}
+			});
+			dia.setNegativeButton("取消", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					result.cancel();
-			}
-		});
-		dia.create();
-		dia.show();
+				}
+			});
+			dia.create();
+			dia.show();
+		}
 		return true;
 	}
 
+	private void handleUexFunction(WebView view, String parseStr) {
+		Log.i("jiangpingping", "CBrowserMainFrame handleUexFunction parseStr "
+				+ parseStr);
+		try {
+			if (!(view instanceof EBrowserView)) {
+				return;
+			}
+			JSONObject json = new JSONObject(parseStr);
+			String uexName = json.optString("uexName");
+			String method = json.optString("method");
+			JSONArray jsonArray = json.getJSONArray("args");
+			JSONArray typesArray = json.getJSONArray("types");
+			int length = jsonArray.length();
+			String[] args = new String[length];
+			for (int i = 0; i < length; i++) {
+				String type = typesArray.getString(i);
+				String arg = jsonArray.getString(i);
+				if ("undefined".equals(type) && "null".equals(arg)) {
+					args[i] = null;
+				} else {
+					args[i] = arg;
+				}
+			}
 
+			WidgetOneApplication app = (WidgetOneApplication) mContext
+					.getApplicationContext();
+			ThirdPluginMgr tpm = app.getThirdPlugins();
+			Map<String, ThirdPluginObject> enginePlugins = tpm
+					.getPlugins();
+			ThirdPluginObject object = enginePlugins.get(uexName);
+			if (object != null && !TextUtils.isEmpty(method)
+					&& object.jmethod.containsKey(method)) {
+				EBrowserView browserView = (EBrowserView) view;
+				EUExManager uexManager = browserView.getEUExManager();
+				if (uexManager != null) {
+					EUExBase uexBase = uexManager.getPluginEUExBase(uexName);
+					if (uexBase != null) {
+						if (uexBase.termination()) {
+							return;
+						}
+						Class<?> c = Class.forName(object.jclass);
+						Method m = c.getMethod(method,
+								new Class[] { String[].class });
+						if (null != m) {
+							m.invoke(uexBase, new Object[] { args });
+							Log.i("jiangpingping",
+									"CBrowserMainFrame handleUexFunction invoke");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
     // For Android 3.0-
     public void openFileChooser(ValueCallback<Uri> uploadMsg) {
         ((EBrowserActivity)mContext).setmUploadMessage(uploadMsg);
